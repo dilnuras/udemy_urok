0094ba9fc9324752ab1c8fb590fa709d13df119b
dilnuras <77263709+dilnuras@users.noreply.github.com>
2021-12-06 16:23:51 +0600

    урок 12 консольное приложение

---
+++ script.js
@@ -0,0 +1,24 @@
+const numberOfFilms = +prompt("Сколько фильмов в уже посмотрели?", "");
+
+const personalMovieDB = {
+
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("Насколько вы оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("Насколько вы оцените его?", "");
+
+
+personalMovieDB.movies[a] = b;
+
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
